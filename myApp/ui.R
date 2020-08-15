7b63cb98a9223046c26dbe9d56646e4b8c3d86ab
deekshant1 <37878690+deekshant1@users.noreply.github.com>
2020-08-15 19:22:27 +0530

    Add files via upload

--- myApp/ui.R
+++ myApp/ui.R
@@ -10,7 +10,10 @@ shinyUI(fluidPage(
             sliderInput("slider1", "Time", 0, 21, value = c(8, 14), step = 2), 
             checkboxInput("show_xlab", "Show/Hide X axis Label", value = TRUE),
             checkboxInput("show_ylab", "Show/Hide Y axis Label", value = TRUE), 
-            checkboxInput("show_title", "Show/Hide Title Label", value = TRUE)
+            checkboxInput("show_title", "Show/Hide Title Label", value = TRUE),
+            br(),
+            p(strong(em("Documentation:",a("Chick Weight plot",href="Readme.html")))),
+            p(strong(em("Github repository:",a("Shiny App - Course Project - Developing Data Products",href="https://github.com/deekshant1/Developing_Data_Products"))))
         ),
         mainPanel(
             h3("Plot"),
